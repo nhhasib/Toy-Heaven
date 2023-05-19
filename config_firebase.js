f4f39dec5afd7ad354fc53bd07bd90efd0c7dd3c
nhhasib <nafiul.hasan1997@gmail.com>
2023-05-20 00:54:40 +0600

    Footer added

---
+++ config_firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAQtgmpJYFjU3Ew2iwDcdrai6QaIToFSB0",
+  authDomain: "toy-heaven-3e4ff.firebaseapp.com",
+  projectId: "toy-heaven-3e4ff",
+  storageBucket: "toy-heaven-3e4ff.appspot.com",
+  messagingSenderId: "795239081200",
+  appId: "1:795239081200:web:3eb843bc3017176db18e35"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
