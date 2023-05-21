b0b86fb71e9662cd4fce831d4bb0a23b78dd7f76
nhhasib <nafiul.hasan1997@gmail.com>
2023-05-21 22:25:22 +0600

    tab category added

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
-
+import { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import ToyCard from './ToyCard';
+
 const Categories = () => {
   const [datas, setDatas] = useState();
+  const [filterData, setFilterData] = useState();
 
   useEffect(() => {
     fetch('http://localhost:5000/all-toys')
@@ -11,26 +13,45 @@ const Categories = () => {
       .then(data => setDatas(data))
   },[])
   const handletab1 = () => {
-    const filterData = datas.filter(data => data.category == 'race_cars');
+    const filterData = datas.filter(data => data.category == 'RaceCars');
     console.log(filterData)
+    setFilterData(filterData)
+  }
+  const handletab2 = () => {
+    console.log(datas)
+    const filterData = datas.filter(data => data.category == 'RemoteControlCars');
+    console.log(filterData)
+    setFilterData(filterData)
+  }
+  const handletab3 = () => {
+    console.log(datas)
+    const filterData = datas.filter(data => data.category == 'EmergencyVehicles');
+    console.log(filterData)
+    setFilterData(filterData)
   }
 
   return (
-  <Tabs className="text-center">
+  <Tabs className="text-center my-10 py-10">
     <TabList>
       <Tab onClick={handletab1}>Race Cars</Tab>
-      <Tab>Title 2</Tab>
-      <Tab>Title 2</Tab>
+      <Tab onClick={handletab2}>Remote Control Cars</Tab>
+      <Tab onClick={handletab3}>Emergency Vehicles</Tab>
     </TabList>
 
     <TabPanel>
-      
+        <div className='grid grid-cols-3 my-6'>
+        {filterData?.map(data=><ToyCard key={data._id} data={data}></ToyCard>)}
+      </div>
     </TabPanel>
     <TabPanel>
-      <h2>Any content 2</h2>
+    <div className='grid grid-cols-3 my-6'>
+        {filterData?.map(data=><ToyCard key={data._id} data={data}></ToyCard>)}
+      </div>
     </TabPanel>
     <TabPanel>
-      <h2>Any content 2</h2>
+    <div className='grid grid-cols-3 my-6'>
+        {filterData?.map(data=><ToyCard key={data._id} data={data}></ToyCard>)}
+      </div>
     </TabPanel>
   </Tabs>
 );
--- src/components/categories/ToyCard.jsx
+++ src/components/categories/ToyCard.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ToyCard = () => {
+const ToyCard = ({ data }) => {
+    const { _id,img,name,price,rating} = data;
     return (
         <div>
-            
+            <div className="card w-96 h-[450px] bg-base-100 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img src={img} alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+                    <h2 className="card-title">{ name}</h2>
+                    <p>Price: ${ price}</p>
+                    <p>Ratings: { price}</p>
+    <div className="card-actions">
+      <Link to={`/all-toys/${_id}`}><button className="btn btn-secondary">View Details</button></Link>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
