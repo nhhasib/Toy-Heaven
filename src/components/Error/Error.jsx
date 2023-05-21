7d3299dac010d06365f9bffa07037387e91f86d1
nhhasib <nafiul.hasan1997@gmail.com>
2023-05-21 21:41:46 +0600

    error page added

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import img from '../../assets/images/error.jpg'
+
+const Error = () => {
+    return (
+        <div className='w-full'>
+           <img src={img} alt="" /> 
+        </div>
+    );
+};
+
+export default Error;
